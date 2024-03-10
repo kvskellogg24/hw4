cceceecca8158df65fcdbe9ba2c52b1663ca2936
Shiv Viswanathan <shiv.viswanathan@kellogg.northwestern.edu>
2024-03-10 10:22:49 +0000

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,16 @@ class UsersController < ApplicationController
   def new
   end
 
+  def show
+    @user = User.find_by({"id" => params["id"]})
+  end
+  
   def create
     @user = User.new
     @user["username"] = params["username"]
     @user["email"] = params["email"]
     @user["password"] = params["password"]
     @user.save
-    redirect_to "/"
+    redirect_to "/users/#{@user["id"]}"
   end
 end
